2d81bbd1422e8764a714ca3d53a936e4a0cb33af
Abraham Jones <56662623+abejones26@users.noreply.github.com>
2022-12-05 19:41:30 -0600

    Reviewed input function as prompt

--- LearnPython/quick_python.py
+++ LearnPython/quick_python.py
@@ -1,71 +1,14 @@
-# Creates a variable with a string "Frankfurter"
-title = "Frankfurter"
-print(title)
+# Collects the user's input for the prompt "What is your name?"
+name = input("What is your name? ")
 
-# Creates a variable with an integer 80
-years = 80
-yr_str = "80"
-yr_cst = int("80")
-yr_str_cst = str(yr_cst)
+# Collects the user's input for the prompt "How old are you?" and converts the string to an integer.
+age = int(input("How old are you? "))
 
-print(years)
-print(yr_str)
-print(yr_cst)
-print(yr_str_cst)
+# Collects the user's input for the prompt "Is input truthy?" and converts it to a boolean. Note that non-zero,
+#   non-empty objects are truth-y.
+trueOrFalse = bool(input("Is the input truthy? "))
 
-# Creates a variable with the boolean value of True
-expert_status = True
-print(expert_status)
-
-# Prints a statement adding the variable
-print("Nick is a professional " + title)
-
-# Convert the integer years into a string and prints
-print("He has been coding for " + str(years) + " years")
-
-# Converts a boolean into a string and prints
-print("Expert status: " + str(expert_status))
-
-# An f-string accepts all data types without conversion
-print(f"Expert status2: {expert_status}")
-
-# Use good naming convention
-tbl_ = 'table'
-df_tbl_ = 'More than one table combine'
-stg_ = 'Staging table'
-stg_df_tbl = 'Staging table in a dataflow'
-
-# How to name multiple variables at once
-Kendall, Fab, Bryan = "Kendall", "Fab", "Bryan"
-print(Kendall)
-print(Fab)
-print(Bryan)
-
-# Create a variable called 'name' that holds a string
-name = "Abraham"
-
-# Create a variable called 'country' that holds a string
-
-# Create a variable called 'age' that holds an integer
-
-# Create a variable called 'hourly_wage' that holds an integer
-
-# Calculate the daily wage for the user
-
-# Create a variable called 'satisfied' that holds a boolean
-
-
-# Print out "Hello <name>!"
-print("Hello " + + "!")
-
-# Print out what country the user entered
-print("You live in " +)
-
-# Print out the user's age
-print("You are " + str() + " years old")
-
-# With an f-string, print out the daily wage that was calculated
-print(f"You make { } per day")
-
-# With an f-string, print out whether the users were satisfied
-print(f"Are you satisfied with your current wage? { }")
+# Creates three print statements that respond with the output.
+print("My name is " + str(name))
+print("I will be " + str(age + 1) + " next year.")
+print("The input was converted to " + str(trueOrFalse))
