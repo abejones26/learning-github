640a4c9f2888ec018ff3664486c031723f5af0d1
Abraham Jones <56662623+abejones26@users.noreply.github.com>
2022-12-05 19:45:20 -0600

    Reviewed student activity

--- LearnPython/quick_python.py
+++ LearnPython/quick_python.py
@@ -12,3 +12,20 @@ trueOrFalse = bool(input("Is the input truthy? "))
 print("My name is " + str(name))
 print("I will be " + str(age + 1) + " next year.")
 print("The input was converted to " + str(trueOrFalse))
+
+# Take input of you and your neighbor
+your_first_name =
+neighbor_first_name =
+
+# Take how long each of you have been coding
+months_you_coded =
+months_neighbor_coded =
+
+# Add total month
+
+total_months_coded = int() + int()
+
+# Print results
+print("I am " + your_first_name + " and my neighbor is " + neighbor_first_name)
+print("Together we have been coding for " +
+      str(total_months_coded) + " months!")
