6ff07a38098f6fe41eb4c460ed0b6723320b7263
Abraham Jones <56662623+abejones26@users.noreply.github.com>
2022-12-05 20:58:27 -0600

    Reviewed condictions

--- LearnPython/quick_python.py
+++ LearnPython/quick_python.py
@@ -1,31 +1,93 @@
-# Collects the user's input for the prompt "What is your name?"
-name = input("What is your name? ")
+x = 1
+y = 10
 
-# Collects the user's input for the prompt "How old are you?" and converts the string to an integer.
-age = int(input("How old are you? "))
+# Checks if one value is equal to another
+if x == 1:
+    print("x is equal to 1 - Abe is smart")
 
-# Collects the user's input for the prompt "Is input truthy?" and converts it to a boolean. Note that non-zero,
-#   non-empty objects are truth-y.
-trueOrFalse = bool(input("Is the input truthy? "))
+# Checks if one value is NOT equal to another
+if y != 1:
+    print("y is not equal to 1")
 
-# Creates three print statements that respond with the output.
-print("My name is " + str(name))
-print("I will be " + str(age + 1) + " next year.")
-print("The input was converted to " + str(trueOrFalse))
+# Checks if one value is less than another
+if x < y:
+    print("x is less than y")
 
-# Take input of you and your neighbor
-your_first_name =
-neighbor_first_name =
+# Checks if one value is greater than another
+if y > x:
+    print("y is greater than x")
 
-# Take how long each of you have been coding
-months_you_coded =
-months_neighbor_coded =
+# Checks if a value is less than or equal to another
+if x >= 1:
+    print("x is greater than or equal to 1")
 
-# Add total month
+# Checks for two conditions to be met using "and"
+if x == 1 and y == 10:
+    print("Both values returned true")
 
-total_months_coded = int() + int()
+# Checks if either of two conditions is met
+if x < 45 or y < 5:
+    print("One or more of the statements were true")
 
-# Print results
-print("I am " + your_first_name + " and my neighbor is " + neighbor_first_name)
-print("Together we have been coding for " +
-      str(total_months_coded) + " months!")
+# Nested if statements
+if x < 10:
+    if y < 5:
+        print("x is less than 10 and y is less than 5")
+    elif y == 5:
+        print("x is less than 10 and y is equal to 5")
+    else:
+        print("x is less than 10 and y is greater than 5")
+
+
+# 1. oooo needs some work
+x =
+if 2 * x > 10:
+    print("Question 1 works!")
+else:
+    print("oooo needs some work")
+
+# 2. Question 2 works!
+x =
+if len("Dog") < x:
+    print("Question 2 works!")
+else:
+    print("Still missing out")
+
+# 3. GOT QUESTION 3!
+x =
+y =
+if (x ** 3 >= y) and (y ** 2 < 26):
+    print("GOT QUESTION 3!")
+else:
+    print("Oh good you can count")
+
+# 4. Dan is in group three
+name =
+group_one = ["Greg", "Tony", "Susan"]
+group_two = ["Gerald", "Paul", "Ryder"]
+group_three = ["Carla", "Dan", "Jefferson"]
+
+if name in group_one:
+    print(name + " is in the first group")
+elif name in group_two:
+    print(name + " is in group two")
+elif name in group_three:
+    print(name + " is in group three")
+else:
+    print(name + " does not have a group")
+
+# 5. Can ride bumper cars
+height =
+age =
+adult_permission =
+
+if (height > 70) and (age >= 18):
+    print("Can ride all the roller coasters")
+elif (height > 65) and (age >= 18):
+    print("Can ride moderate roller coasters")
+elif (height > 60) and (age >= 18):
+    print("Can ride light roller coasters")
+elif ((height > 50) and (age >= 18)) or ((adult_permission) and (height > 50)):
+    print("Can ride bumper cars")
+else:
+    print("Stick to lazy river")
